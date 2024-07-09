a10bab0a3e2633c5dcd1fe6c825b494f27f01591
Lukas <55952258+xzLxkas@users.noreply.github.com>
2024-07-09 19:03:45 +0200

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Führe die Befehle aus
+pacman -Syu --noconfirm
+pacman -Rns --noconfirm
+
+yay -Syu --noconfirm
+yay -Rns --noconfirm
+
+# Überprüfe den Status des vorherigen Befehls
+if [ $? -eq 0 ]; then
+    # Befehle erfolgreich ausgeführt
+    echo -e "\e[1;32mUpdates abgeschlossen\e[0m"  # Ausgabe in grüner, fetter Farbe
+else
+    # Fehler bei der Ausführung der Befehle
+    echo "Fehler bei der Ausführung der Updates" >&2
+fi
+#Zeige version und Hostname
+inxi -Sz
+hostname
